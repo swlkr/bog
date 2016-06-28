a8fd3dab109443cc03058c364d0332b5f71ebac3
Sean Walker <swlkr.rbl@gmail.com>
2016-06-27 22:43:04 -0700

    Add api status route

--- src/clj/bog/routes.clj
+++ src/clj/bog/routes.clj
@@ -3,12 +3,20 @@
             [compojure.core :refer [GET defroutes]]
             [compojure.route :refer [resources not-found]]))
 
+; api routes
+(defroutes api-routes
+  (GET "/api/status" request
+    {:status 200
+     :body {:message "alive"}}))
+
+; client routes
 (def client-response
   {:status 200
    :headers {"Content-Type" "text/html; charset=utf-8"}
    :body (io/input-stream (io/resource "public/index.html"))})
 
 (defroutes routes
+  api-routes
   (GET "/" _ client-response)
   (resources "/")
   (not-found (slurp (io/resource "public/404.html"))))
