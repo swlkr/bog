1040fc4d79e4e196ea9e55ecad9522cf9f56bf46
Sean Walker <swlkr.rbl@gmail.com>
2016-06-27 22:42:46 -0700

    Wrap requests and responses as json

--- src/clj/bog/server.clj
+++ src/clj/bog/server.clj
@@ -4,12 +4,15 @@
             [ring.middleware.logger :refer [wrap-with-logger]]
             [environ.core :refer [env]]
             [org.httpkit.server :refer [run-server]]
-            [bog.routes :refer [routes]])
+            [bog.routes :refer [routes]]
+            [ring.middleware.json :refer [wrap-json-body wrap-json-response]])
   (:gen-class))
 
 (def http-handler
   (-> routes
       (wrap-defaults api-defaults)
+      (wrap-json-body {:keywords? true})
+      wrap-json-response
       wrap-with-logger
       wrap-gzip))
 
