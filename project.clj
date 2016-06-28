2185c1ed166994a79ae96d18452645edded86e5b
Sean Walker <swlkr.rbl@gmail.com>
2016-06-27 22:10:27 -0700

    Change description and url in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject bog "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "An overengineered blog engine made with love"
+  :url "https://adventurewalker.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
