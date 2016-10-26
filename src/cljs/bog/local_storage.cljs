6093794ac586debf3a96ad06bdbd88354b0fb687
Sean Walker <swlkr.rbl@gmail.com>
2016-10-26 07:41:29 -0700

    Remove unused code

--- src/cljs/bog/local_storage.cljs
+++
@@ -1,7 +0,0 @@
-(ns bog.local-storage)
-
-(defn set-item! [key value]
-  (.setItem (.-localStorage js/window) key value))
-
-(defn get-item [key]
-  (.getItem (.-localStorage js/window) key))
--- src/cljs/bog/tokens.cljs
+++
@@ -1,6 +0,0 @@
-(ns bog.tokens
-  (:require [bog.local-storage :as storage]
-            [bog.app-state :refer [app-state]]))
-
-(defn get-token []
-  (storage/get-item "access-token"))
